21033060260bb401b2175f2af3b30e487454543b
Yogesh More <yogesh.more2098@gmail.com>
2023-06-11 14:34:06 +0530

    Added: Generate a random two dice numbers.

---
+++ Sequence-Practice-Problems/TwoDice.js
@@ -0,0 +1,9 @@
+// 3. Use REPL – Add two Random Dice Number and Print the Result
+
+Math.random();
+dice1=((Math.floor(Math.random() * 6) + 1));
+console.log("Roll the dice 1: "+dice1)
+dice2=((Math.floor(Math.random() * 6) + 1));
+console.log("Roll the dice 2: "+dice2)
+sum=((dice1+dice2));
+console.log("Addition of two random dice number: "+sum)
